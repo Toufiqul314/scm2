a538dee2cf7e113f65fca270d16685934b661bf2
Toufiqul314 <islamtusher4@gmail.com>
2024-08-25 13:28:40 +0600

    validation

--- src/main/java/com/scm/scm2/forms/UserForm.java
+++ src/main/java/com/scm/scm2/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.scm.scm2.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.Setter;
 
@@ -11,9 +14,17 @@ import lombok.Setter;
 @ToString
 public class UserForm {
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 3,message = "Min 3 characters is required")
     private String name;
+    @Email(message = "Invalid email address")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6,message = "Min 6 characters is required")
     private String password;
+    @NotBlank(message = "About is required")
+    @Size(message = "About is required")
     private String about;
+    @Size(min = 8,max = 11,message = "Invalid Phone Number")
     private int phoneNumber;
 }
